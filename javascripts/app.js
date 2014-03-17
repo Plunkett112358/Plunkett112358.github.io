d70759cf0df1ead1409a14d57f1d5f9be45fbaaf
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-03-17 19:55:06 -0400

    changing layout, adding jquery easing

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,42 @@
-console.log('feed me javascripts')
+$(function() {
+
+$(document).ready(function() {
+   $('#my_carousel').carousel('pause');
+});
+
+    $('a[href*=#]:not([href=#])').not(".carousel-control").click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+        return false;
+    });
+
+    $(".about_fade_in").on("click", function() {
+        $("#about_content").hide();
+        $("#about_content").fadeIn( 2000 );
+    });
+
+     $("#contact_title_fade_in").on("click", function() {
+        $("#contact-title").hide();
+        $("#contact-title").fadeIn( 3000 );
+    });
+
+     $(".contact_fade_in").on("click", function() {
+        $("#holler").hide();
+        $("#holler").fadeIn( 3000 );
+    });
+
+
+    $(".github_link").find("i").hover(function() {
+        $(this).css("font-size", "800%");
+    }, function(){
+        $(this).css("font-size", "500%");
+    });
+});
